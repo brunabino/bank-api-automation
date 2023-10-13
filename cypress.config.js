64844eaa99d8ed028703e4a7e04ff03b7dbc2908
bhirosse <brunabino.h@gmail.com>
2023-10-12 22:11:00 -0400

    add git ignore and automation project

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,20 @@
 const { defineConfig } = require("cypress");
 
+
 module.exports = defineConfig({
+
+  reporter: 'cypress-mochawesome-reporter',
+
   e2e: {
+
+    baseUrl: 'https://www.bankofcanada.ca/valet/',
+
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+
+      require('cypress-mochawesome-reporter/plugin')(on);
+
     },
+
   },
+
 });
